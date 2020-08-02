831c82ea804fbf2e6eddf59ea90a9551552a9b7f
yogeshwaran01 <yogeshin247@gmail.com>
2020-08-02 11:34:26 +0530

    Users help command get some updates

--- help.py
+++ help.py
@@ -1,22 +1,32 @@
-def helper():
+def helper(username):
     result = """
+
+Welcome {}, I am Excel, a Bot I have some features   
+
 Main usage:
+==========
     (TRANSLATIONS)
     TAMIL --- > ENGLISH
     ENGLISH --- > TAMIL
+-----------------------------------------
 Some features:
-    ta any topic --> short passage about topic in          tamil
-    en any topic --> short passage about topic in          english
+=============
+    ta any topic --> short passage about topic in tamil
+    en any topic --> short passage about topic in english
     (Read the passage and understand the meanings)
+-----------------------------------------
 Some additional features:
+========================
     * your_name ==> Gives Expand form of your name and its describe your character
                                         (ex: * dhoni)
     # name_1 name_2 ==> Gives FLAMES between name_1 and name_2
                                         (ex: # dhoni virat)
+-----------------------------------------                                        
 About:
-    author ==> Gives the name of this Bot           Creator
-    name ==> Gives the name of this bot
-    username ==> Gives the username of this bot
-    help ==> To see the Descriptions
-    """
+=====
+    !author ==> Gives the name of this Bot           Creator
+    !name ==> Gives the name of this bot
+    !username ==> Gives the username of this bot
+    !help ==> To see the Descriptions
+    """.format(username)
     return result
